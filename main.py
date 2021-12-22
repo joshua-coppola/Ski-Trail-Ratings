47602647ac764bcbb4a379b44e88421b23edbf9c
joshua-coppola <joshua.coppola99@gmail.com>
2021-12-22 14:09:14 -0500

    converted everything to functions, added calculation to find distance between each pair of coordinates

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
+from numpy import NAN
 import pandas as pd
-from pandas.core.arrays.categorical import contains
+import haversine as hs
 
-raw_table = pd.read_csv('rimrock-415690.gpx').to_numpy()
+# accepts a gpx filename and returns a dataframe 
+# with 2 columns: lat/lon pairs and elevation (in feet)
+def load_data(filename):
+    raw_table = pd.read_csv(filename).to_numpy()
+    df = pd.DataFrame()
+    coordinates = []
+    elevation = []
+    for row in raw_table:
+        row = str(row)
+        if 'trkpt lat' in row:
+            split_row = row.split('"')
+            coordinates.append((float(split_row[1]),float(split_row[3])))
+        if 'ele' in row:
+            elevation.append(float(row.split('>')[1].split('<')[0]))
+    df['coordinates'] = coordinates
+    df['elevation'] = elevation
+    return df
 
-df = pd.DataFrame()
-lat = []
-lon = []
-elevation = []
-for row in raw_table:
-    row = str(row)
-    if 'trkpt lat' in row:
-        split_row = row.split('"')
-        lat.append(float(split_row[1]))
-        lon.append(float(split_row[3]))
-    if 'ele' in row:
-        elevation.append(float(row.split('>')[1].split('<')[0]))
-df['lat'] = lat
-df['lon'] = lon
-df['elevation'] = elevation
+#accepts a list of lat/lon pairs and returns a list of distances (in feet)
+def calculate_dist(coordinates):
+    previous_row = (NAN,NAN)
+    distance = []
+    for row in coordinates:
+        distance.append(hs.haversine(row, previous_row,unit=hs.Unit.FEET))
+        previous_row = row
+    return distance
 
+df = load_data('rimrock-415690.gpx')
+df['distance'] = calculate_dist(df['coordinates'])
 print(df)
