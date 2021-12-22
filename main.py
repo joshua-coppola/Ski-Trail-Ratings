8b574b497b23f58087edf0f5cb014aa0f9806726
joshua-coppola <joshua.coppola99@gmail.com>
2021-12-22 16:07:17 -0500

    added basic visualization, added another test gpx file

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-from numpy import NAN
+from numpy import NAN, absolute
 import pandas as pd
 import haversine as hs
 import math
 import matplotlib.pyplot as plt
 
 # accepts a gpx filename and returns a dataframe
-# with 2 columns: lat/lon pairs and elevation (in meters)
+# with 4 columns: latitude, longitude, lat/lon pairs and elevation (in meters)
 
 
 def load_data(filename):
     raw_table = pd.read_csv(filename).to_numpy()
     df = pd.DataFrame()
+    lat = []
+    lon = []
     coordinates = []
     elevation = []
     for row in raw_table:
         row = str(row)
         if 'trkpt lat' in row:
             split_row = row.split('"')
+            lat.append(float(split_row[1]))
+            lon.append(float(split_row[3]))
             coordinates.append((float(split_row[1]), float(split_row[3])))
         if 'ele' in row:
             elevation.append(float(row.split('>')[1].split('<')[0]))
+    df['lat'] = lat
+    df['lon'] = lon
     df['coordinates'] = coordinates
     df['elevation'] = elevation
     return df
@@ -54,8 +60,13 @@ def calulate_elevation_change(elevation):
 def calculate_slope(elevation_change, distance):
     return [math.degrees(math.atan(x/y)) for x,y in zip(elevation_change, distance)]
 
-df = load_data('rimrock-415690.gpx')
+#df = load_data('rimrock-415690.gpx')
+df = load_data('tuckered-out.gpx')
 df['distance'] = calculate_dist(df['coordinates'])
 df['elevation_change'] = calulate_elevation_change(df['elevation'])
 df['slope'] = calculate_slope(df['elevation_change'], df['distance'])
-print(df)
+
+print(df.slope.min())
+plt.plot(df.lon, df.lat, alpha=.25)
+plt.scatter(df.lon, df.lat, s=8 ,c=abs(df.slope), alpha=1)
+plt.show()
