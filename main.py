0b5c304dd37b039c6794adfa37555a6c8f089504
joshua-coppola <joshua.coppola99@gmail.com>
2022-01-19 15:27:45 -0500

    added command line args

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ import csv
 from genericpath import exists
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys, getopt
 
 import loadData
 import saveData
 
-def gpx():
-    filename = 'gpx/big-bang-409529.gpx'
+def gpx(filename):
+    #filename = 'gpx/big-bang-409529.gpx'
     #filename = 'gpx/rimrock-415690.gpx'
     #filename = 'gpx/tuckered-out.gpx'
     loadData.runGPX(filename)
 
-def osm(mountain='', direction='', save_map=False):
-    if mountain == '':
-        mountain = "breckenridge"
-        direction = 'w'
-        save_map = True
-    
+def osm(mountain='', direction='n', save_map=False):
     print('\nProcessing {}'.format(mountain))
     diff_tuple = loadData.runOSM(mountain, direction, save_map)
     if diff_tuple == -1:
@@ -45,13 +41,60 @@ def bulk_osm(input_csv, save_map = False):
             osm(line[0], line[1], save_map)
             
 
-def osm_barplot(save):
+def barplot(save):
     if not exists('mountain_list.csv'):
         print('Missing cache files, please run bulk_osm or osm and set save_map=True.')
         return
     df = pd.read_csv('mountain_list.csv')
     saveData.create_difficulty_barplot(df, save)
 
-#bulk_osm('mountain_list.csv', True, True)
-#osm_barplot(True)
-osm()
+
+def main(argv):
+    file = ''
+    save_flag = False
+    osm_flag = False
+    csv_flag = False
+    gpx_flag = False
+    bar_flag = False
+    direction = ''
+    try:
+        opts, args = getopt.getopt(argv,"hbso:g:c:d:",["osm=","gpx=","csv=", "direction="])
+    except getopt.GetoptError:
+        print('main.py -[o,g,c] <inputfile> -s')
+        print('main.py -[o,g,c] <inputfile>')
+        print('main.py -b -s')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -[o,g,c] <inputfile> -s')
+            print('main.py -[o,g,c] <inputfile>')
+            print('main.py -b')
+            sys.exit()
+        elif opt in ("-o", "--osm"):
+            file = arg
+            osm_flag = True
+            print()
+        elif opt in ("-g", "--gpx"):
+            file = arg
+            gpx_flag = True
+        elif opt in ("-c", "--csv"):
+            file = arg
+            csv_flag = True
+        elif opt in ("-d", "--direction"):
+            direction = arg
+        elif opt in ("-b"):
+            bar_flag = True
+        elif opt in ("-s"):
+            save_flag = True
+    
+    if csv_flag:
+        bulk_osm(file, save_flag)
+    elif osm_flag:
+        osm(file, direction, save_flag)
+    elif gpx_flag:
+        gpx(file)
+    if bar_flag:
+        barplot(save_flag)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
